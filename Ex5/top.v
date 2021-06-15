c5dc636d926ce7270c487a8a0f23a305d03ceaf3
Cloud User <centos@ip-172-31-6-147.eu-west-2.compute.internal>
2021-06-15 13:08:56 +0000

    1st attempt of Ex5 - finished

--- Ex5/top.v
+++ Ex5/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #5 - Air Conditioning
-// Student Name:
-// Date: 
+// Student Name: Peiyu (Tom) Liu
+// Date: 15 June
 //
 //  Description: In this exercise, you need to an air conditioning control system
 //  According to the state diagram provided in the exercise.
@@ -13,3 +13,38 @@
 //           heating, cooling
 //////////////////////////////////////////////////////////////////////////////////
 
+
+`timescale 1ns / 100ps
+
+module aircon (
+    //Todo: add ports 
+	input clk,	//0 or 1
+	input [4:0] temp,	//temperature in degrees celsius
+	output [1:0] mode	//1st bit is 1 if heating, 2nd bit is 1 if cooling
+    );
+                    
+    //Todo: add registers and wires, if needed
+    	reg [1:0] mode;
+
+    //Todo: add user logic
+	always @ (posedge clk)
+		case(mode)
+			2'b00: begin
+				if (temp <= 5'd18)
+					mode[1] = 1;
+				else if (temp >= 5'd22)
+					mode[0] = 1;
+			end
+			2'b01: begin
+				if (temp <= 5'd20)
+					mode[0] = 0;
+			end
+			2'b10: begin
+				if (temp >= 5'd20)
+					mode[1] = 0;
+			end
+			default: mode = 2'b0;
+		endcase
+			
+      
+endmodule
--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -1,9 +1,83 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #5 - Air Conditioning
-// Student Name:
-// Date: 
+// Student Name: Peiyu (Tom) Liu
+// Date: 15 June
 //
 // Description: A testbench module to test Ex5 - Air Conditioning
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module top_tb();
+    
+//Todo: Parameters
+	parameter CLK_PERIOD = 10; 	//set clock period
+
+//Todo: Registers and wires
+	reg clk;
+	reg [4:0] temp;
+	wire [1:0] mode;
+	reg err;
+
+
+//Todo: Clock generation
+	initial begin
+       		clk = 1'b0;
+       		forever
+         		#(CLK_PERIOD/2) clk=~clk;	//change every half-period
+     	end
+
+//Todo: error checking
+	initial begin
+		err = 0;
+		
+		forever begin
+			#(CLK_PERIOD)
+			if ((temp > 5'd22) && ((mode == 2'b10) || (mode == 2'b00))) begin
+				err = 1;
+				$display("*** TEST FAILED :( *** temp=%d mode(heating_bit, cooling_bit) =%d", temp, mode);
+			end else if ((temp < 5'd18) && ((mode == 2'b01) || (mode == 2'b00))) begin
+				err = 1;
+				$display("*** TEST FAILED :( *** temp=%d mode(heating_bit, cooling_bit) =%d", temp, mode);
+			end else if (mode == 2'b11) begin
+				err = 1;
+				$display("*** INVALID MODE :( *** temp=%d mode(heating_bit, cooling_bit) =%d", temp, mode);
+			end
+		end
+	end
+			
+
+//Todo: Final block & schange variables
+
+	initial begin
+		temp = 5'd20;
+
+		#(CLK_PERIOD*2)
+		temp = 5'd22;
+		#(CLK_PERIOD*2)
+		temp = 5'd25;
+		#(CLK_PERIOD*2)
+		temp = 5'd20;
+		#(CLK_PERIOD*2)
+		temp = 5'd18;
+		#(CLK_PERIOD*2)
+		temp = 5'd19;
+		#(CLK_PERIOD*2)
+		temp = 5'd20;
+		#(CLK_PERIOD*2)
+		if (err==0)
+          		$display("***TEST PASSED!***");
+
+		$finish;
+	end
+
+//Todo: Instantiate counter module
+	aircon top (
+		.clk (clk),
+		.temp (temp),
+		.mode (mode)
+		);	
+	
+
+ 
+endmodule
