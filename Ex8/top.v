5ca0e7fa8f41e710cfbbc89821e207633f7d2e11
Cloud User <centos@ip-172-31-6-147.eu-west-2.compute.internal>
2021-06-16 13:06:49 +0000

    Part 1 - unfinished

--- Ex8/top.v
+++ Ex8/top.v
@@ -1,9 +1,9 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #8  - Simple End-to-End Design
-// Student Name:
-// Date: 
+// Student Name: Peiyu (Tom) Liu
+// Date: 16 June 2021
 //
-//  Description: In this exercise, you need to design an air conditioning systems
+//  Description: In this exercise, you need to design an air conditioning system
 //
 //  inputs:
 //           rst_n, clk_n, clk_p, temperature [4:0]
@@ -14,28 +14,55 @@
 
 
 module top(
-    input clk_p,
-    input clk_n,
+    	input clk_p,
+    	input clk_n,
      //Todo: add all other ports besides clk_n and clk_p 
-   );
+	input rst_n,
+	input [4:0] temperature,
+	output heating,				//1 if on
+	output cooling				//1 if on
+   	);
+
+	//registers
+	reg heating;
+	reg cooling;
     
 
    /* clock infrastructure, do not modify */
         wire clk_ibufds;
 
-    IBUFDS IBUFDS_sysclk (
-	.I(clk_p),
-	.IB(clk_n),
-	.O(clk_ibufds)
-);
+    	IBUFDS IBUFDS_sysclk (
+		.I(clk_p),
+		.IB(clk_n),
+		.O(clk_ibufds)
+	);
 
-     wire clk; //use this signal as a clock for your design
+     	wire clk; //use this signal as a clock for your design
         
-     BUFG bufg_clk (
-	.I  (clk_ibufds),
-	.O  (clk)
-      );
+     	BUFG bufg_clk (
+		.I  (clk_ibufds),
+		.O  (clk)
+      	);
 
 //Add logic here
+	always @ (posedge clk)
+		case({heating,cooling})
+			2'b00: begin
+				if (temperature <= 5'd18)
+					heating = 1;
+				else if (temperature >= 5'd22)
+					cooling = 1;
+			end
+			2'b01: begin
+				if (temperature <= 5'd20)
+					cooling = 0;
+			end
+			2'b10: begin
+				if (temperature >= 5'd20)
+					heating = 0;
+			end
+			default: {heating,cooling} = 2'b0;
+		endcase
+			
 
 endmodule
--- Ex8/top_tb.v
+++ Ex8/top_tb.v
@@ -1,9 +1,85 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #8  - Simple End-to-End Design
-// Student Name:
-// Date: 
+// Student Name: Peiyu (Tom) Liu
+// Date: 16 June 2021
 //
 // Description: A testbench module to test Ex8
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module top_tb();
+    
+//Todo: Parameters
+	parameter CLK_PERIOD = 10; 	//set clock period
+
+//Todo: Registers and wires
+	reg clk;
+	reg [4:0] temperature;
+	wire heating;
+	wire cooling;
+	reg err;
+
+
+//Todo: Clock generation
+	initial begin
+       		clk = 1'b0;
+       		forever
+         		#(CLK_PERIOD/2) clk=~clk;	//change every half-period
+     	end
+
+//Todo: error checking
+	initial begin
+		err = 0;
+		
+		forever begin
+			#(CLK_PERIOD)
+			if ((temperature > 5'd22) && (({heating,cooling} == 2'b10) || ({heating,cooling} == 2'b00))) begin
+				err = 1;
+				$display("*** TEST FAILED: temperature=%d heating=%d cooling=%d) ***", temperature, heating, cooling);
+			end else if ((temperature < 5'd18) && (({heating,cooling} == 2'b01) || ({heating,cooling} == 2'b00))) begin
+				err = 1;
+				$display("*** TEST FAILED: temperature=%d heating=%d cooling=%d) ***", temperature, heating, cooling);
+			end else if ({heating,cooling} == 2'b11) begin
+				err = 1;
+				$display("*** TEST FAILED: temperature=%d heating=%d cooling=%d) ***", temperature, heating, cooling);
+			end
+		end
+	end
+			
+
+//Todo: Final block & schange variables
+
+	initial begin
+		temperature = 5'd20;
+
+		#(CLK_PERIOD*2)
+		temperature = 5'd22;
+		#(CLK_PERIOD*2)
+		temperature = 5'd25;
+		#(CLK_PERIOD*2)
+		temperature = 5'd20;
+		#(CLK_PERIOD*2)
+		temperature = 5'd18;
+		#(CLK_PERIOD*2)
+		temperature = 5'd19;
+		#(CLK_PERIOD*2)
+		temperature = 5'd20;
+		#(CLK_PERIOD*2)
+		if (err==0)
+          		$display("***TEST PASSED!***");
+
+		$finish;
+	end
+
+//Todo: Instantiate module
+	top top (
+		.clk (clk),
+		.temperature (temperature),
+		.heating (heating),
+		.cooling (cooling)
+		);	
+	
+
+ 
+endmodule
